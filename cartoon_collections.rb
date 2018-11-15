32618812c6d9729a1d8ba2303522fe7ede09f06e
David Anderson <matandersonlv@hotmail.com>
2018-11-15 19:17:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,5 @@
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    puts "Hey! No block was given!"
-  end
+def roll_call_dwarves(array)
+  # Your code here
 end
 
 def summon_captain_planet# code an argument here
