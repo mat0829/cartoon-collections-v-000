1fdf98d1c87fd145050753a1edea10a58346caf5
David Anderson <matandersonlv@hotmail.com>
2018-11-15 19:00:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves()
   collection = ["Doc", "Dopey", "Bashful", "Grumpy"]
 my_each(collection) do |i|
-  puts collection
+  puts collection.each_with_index
 end
 
 def summon_captain_planet# code an argument here
