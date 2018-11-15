4ebe306f653f3685e5bbeb0668e6794ceb944c3b
David Anderson <matandersonlv@hotmail.com>
2018-11-15 19:20:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,10 @@
 def roll_call_dwarves(array)
   i = 0
   
-  while i > array.length
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
 def summon_captain_planet# code an argument here
